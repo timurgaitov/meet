9f01d0325ca6745428467de32186dea1c797bda7
Timur Gaitov <timur.gaitov@gmail.com>
2025-08-02 12:26:24 +0700

    9

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
